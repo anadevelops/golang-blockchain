1210e006d1790e00b0bdb8ad98a7ebce199ff4f4
Ana Clara Stupp <ana.stupp02@gmail.com>
2024-03-06 11:20:21 -0300

    .

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"blockchain"
 	"fmt"
+	"strconv"
+
+	"github.com/anadevelops/golang-blockchain/blockchain"
 )
 
 func main() {
@@ -12,9 +14,13 @@ func main() {
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
